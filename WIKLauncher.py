e3be8fa1a830dd63604c029a521468662c7a3e2e
dpslwk <dps.lwl@gmail.com>
2013-07-08 12:18:45 +0100

    New WIKLauncher wrapper

---
+++ WIKLauncher.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Wireless Inventers Kit Launcher
+    Ciseco Ltd. Copyright 2013
+    
+    Author: Matt Lloyd
+    
+    This code is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    
+    Quick Wrapper to move us into the Python/ directory
+"""
+import os
+import sys
+args = sys.argv[:]
+
+args.insert(0, sys.executable)
+if sys.platform == 'win32':
+    args = ['"%s"' % arg for arg in args]
+
+os.chdir('Python/')
+os.execv(sys.executable, args)
