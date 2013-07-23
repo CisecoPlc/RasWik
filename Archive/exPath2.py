38af4e18a9bde757fdfb39b7ed811dfc9e1786f0
dpslwk <dps.lwl@gmail.com>
2013-07-23 14:36:22 +0100

    Removed Archive Examples

--- Archive/exPath2.py
+++
@@ -1,3 +0,0 @@
-import inspect, os
-print inspect.getfile(inspect.currentframe()) # script filename (usually with path)
-print os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) # script directory
--- Archive/exinspect.py
+++
@@ -1,7 +0,0 @@
-import inspect
-import os
-
-print inspect.getfile(inspect.currentframe())
-print os.path.abspath(inspect.getfile(inspect.currentframe()))
-print os.path.dirname(inspect.getfile(inspect.currentframe()))
-print os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
